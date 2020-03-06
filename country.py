b58531f1efb77d1c6f2ea283525c900d9975f16a
SNKotV <48939733+SNKotV@users.noreply.github.com>
2020-03-07 00:41:29 +0300

    "test"

--- country.py
+++ country.py
@@ -1,6 +1,9 @@
 import pygame
 import random
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 import os
+
 
 class Country:
 
@@ -9,8 +12,9 @@ class Country:
         self.population = 0
         self.position = ()
         self.size = ()
-        self.borders_image = ...
-        self.colored_image = ...
+        self.borders_image = None
+        self.colored_image = None
+        self.country_shape_polygon = None
         self.sick = 0
         self.is_sick = False
 
@@ -28,12 +32,28 @@ class Country:
         self.colored_image = pygame.transform.scale(self.colored_image, self.size)
         self.colored_image.set_alpha(0)
 
-    def update(self):
+    def set_polygon(self, points):
+        self.country_shape_polygon = Polygon(points)
+
+    def update(self, speed):
         if self.is_sick:
-            self.sick = min(self.population, self.sick + random.randint(0, self.sick))
+            sick = self.sick + 10000
+            self.sick = min(self.population, sick)
             alpha = self.sick / self.population * 255
             self.colored_image.set_alpha(alpha)
+
+            print(alpha)
 
     def draw(self, win):
         win.blit(self.borders_image, self.position)
         win.blit(self.colored_image, self.position)
+
+    def is_selected(self, x, y):
+        x -= self.position[0]
+        y -= self.position[1]
+        point = Point((x, y))
+        return self.country_shape_polygon.contains(point)
+
+    def infect(self):
+        self.is_sick = True
+        self.sick = 1
--- game.py
+++ game.py
@@ -4,7 +4,17 @@ import country
 
 
 def countries_init():
-    return []
+    cntrs = []
+    cnt = country.Country("Test country")
+    cnt.population = 5000000
+    cnt.is_sick = True
+    cnt.set_position((100, 100))
+    cnt.set_size((900, 600))
+    cnt.load_images("test.png", "testcol.png")
+    points = [(0, 0), (0, 500), (500, 500), (500, 0)]
+    cnt.set_polygon(points)
+    cntrs.append(cnt)
+    return cntrs
 
 
 class Game:
@@ -12,13 +22,18 @@ class Game:
         self.width = 1200
         self.height = 800
         self.win = pygame.display.set_mode((self.width, self.height))
+        self.bg = pygame.image.load(os.path.join("imgs", "bg.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
         self.countries = countries_init()
+        self.selected_county = None
+        self.is_country_infected = False
         self.days_passed = 0
         self.speed = 1
 
     def run(self):
         run = True
         while run:
+            clock = pygame.time.Clock()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -28,18 +43,35 @@ class Game:
                     if pressed_keys[pygame.K_F4] and pressed_keys[pygame.K_LALT]:
                         run = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    print(pygame.mouse.get_pos())
+                    for cntry in self.countries:
+                        x = pygame.mouse.get_pos()[0]
+                        y = pygame.mouse.get_pos()[1]
+                        if cntry.is_selected(x, y):
+                            #
+                            cntry.infect()
+                            self.is_country_infected = True
+                            #
+                            self.selected_county = cntry
+                            break
 
             self.update()
             self.draw()
 
+            clock.tick(30)
+
         pygame.quit()
 
     def update(self):
-        for cntry in self.countries:
-            cntry.update()
+        if self.is_country_infected:
+            self.days_passed += self.speed
+
+            for cntry in self.countries:
+                cntry.update(self.speed)
+
+
 
     def draw(self):
+        self.win.blit(self.bg, (0, 0))
 
         for cntry in self.countries:
             cntry.draw(self.win)
